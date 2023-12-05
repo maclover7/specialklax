a12cdd5a09f361e3c537ed19d944380639f7bf07
Jon Moss <jmoss@syracuse.com>
2023-12-05 16:37:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { writeFile } = require('fs/promises');
+
+const getOCWAMeetings = () => {
+  fetch("https://go.boarddocs.com/ny/ocwa/Board.nsf/BD-GetMeetingsList?open&0.9541476706708205", {
+    "body": "current_committee_id=A9QN5Z5E5509",
+    "method": "POST"
+  })
+  .then(r => r.json())
+  .then(r => r.map(meeting => `${meeting.numberdate} ${meeting.name}`))
+  .then(m => writeFile('ocwa-meetings.json', JSON.stringify(m)));
+};
+
+[
+  getOCWAMeetings
+].forEach(fn => fn());
